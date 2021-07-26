7903cdd0f9f5e9ffe7c7b713bc6ae57963645294
Frameworker <20012001amiramir@gmail.com>
2021-07-26 23:57:14 +0300

    init
    amir

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Hey ti guy'),
+          ),
+          body: const SimpleWidget(),
+        )
+    );
+  }
+}
+
+class SimpleWidget extends StatelessWidget {
+  const SimpleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+
+}
+
